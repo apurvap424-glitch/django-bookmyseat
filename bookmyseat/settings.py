f22046d291287183c6d5d8d67ff390f97e51790d
Apurva Patil <apurvap424@gmail.com>
2026-08-06 23:04:22 +0530

    fix template location

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -63,7 +63,7 @@ LOGIN_URL='/login/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
